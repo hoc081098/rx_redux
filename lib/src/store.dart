df2bfd90df7aa4cf5127f767320b8cefb4c763d9
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2020-08-18 22:19:07 +0700

    updated store

--- lib/src/store.dart
+++ lib/src/store.dart
@@ -1,23 +1,28 @@
 import 'dart:async';
 
+import 'package:meta/meta.dart';
 import 'package:rx_redux/rx_redux.dart';
 
-///
+/// Rx Redux Store.
+/// Redux store based on [Stream].
 class Store<A, S> {
   final _action = StreamController<A>.broadcast();
 
   StreamSubscription _subscription;
   S _state;
   Stream<S> _stateStream;
+  bool Function(S previous, S next) _equals;
 
-  ///
-  Store(
-    S initialState,
-    List<SideEffect<A, S>> sideEffects,
-    Reducer<A, S> reducer,
+  /// Construct a [Store]
+  Store({
+    @required S initialState,
+    @required List<SideEffect<A, S>> sideEffects,
+    @required Reducer<A, S> reducer,
     void Function(Object, StackTrace) handleError,
-  ) {
+    Function(S previous, S next) equals,
+  }) {
     _state = initialState;
+    _equals = _equals;
 
     _stateStream = _action.stream.reduxStore<S>(
       initialStateSupplier: () => initialState,
@@ -32,89 +37,88 @@ class Store<A, S> {
     );
   }
 
+  /// Get stream of states.
   ///
-  Stream<S> get stateStream async* {
-    yield _state;
-    yield* _stateStream;
+  /// The stream skips states if they are equal to the previous state.
+  /// Equality is determined by the provided [equals] method. If that is omitted,
+  /// the '==' operator is used.
+  Stream<S> get stateStream {
+    final stream = () async* {
+      yield _state;
+      yield* _stateStream;
+    }();
+    return stream.distinct(_equals);
   }
 
-  ///
+  /// Synchronously get current state.
   S get state => _state;
 
-  ///
+  /// Dispatch action to store.
   void dispatch(A action) => _action.add(action);
 
-  ///
+  /// Dispose all resources.
   Future<void> dispose() async {
-    await _action.close();
     await _subscription.cancel();
+    await _action.close();
   }
 }
 
-enum Action { a1, a2, a3, b1, b2, b3 }
+enum _Action { a1, a2, a3, b1, b2, b3, b0 }
 
 void main() async {
-  try {
-    final store = Store<Action, int>(
-      0,
-      [
-        (action, getState) => action
-            .where((event) => event == Action.a1)
-            .asyncExpand((event) =>
-                Stream.periodic(const Duration(seconds: 1), (_) => Action.b1)
-                    .take(1)),
-        (action, getState) => action
-            .where((event) => event == Action.a2)
-            .asyncExpand((event) => Stream.periodic(
-                const Duration(milliseconds: 500), (_) => Action.b2).take(1)),
-        (action, getState) => action
-            .where((event) => event == Action.a3)
-            .asyncExpand((event) => Stream.periodic(
-                const Duration(milliseconds: 300), (_) => Action.b3).take(1)),
-        (action, getState) {
-          return Stream.error(Exception());
-        },
-      ],
-      (state, action) {
-        switch (action) {
-          case Action.b1:
-            return state + 1;
-          case Action.b2:
-            return state + 2;
-          case Action.b3:
-            return state + 3;
-          default:
-            return state;
-        }
-      },
-      (e, s) => print('Oh no $e'),
-    );
+  final store = Store<_Action, int>(
+    initialState: 0,
+    sideEffects: [
+      (action, getState) => action
+          .where((event) => event == _Action.a1)
+          .asyncExpand((event) =>
+              Stream.periodic(const Duration(seconds: 1), (_) => _Action.b1)
+                  .take(1)),
+      (action, getState) => action
+          .where((event) => event == _Action.a2)
+          .asyncExpand((event) => Stream.periodic(
+              const Duration(milliseconds: 500), (_) => _Action.b2).take(1)),
+      (action, getState) => action
+          .where((event) => event == _Action.a3)
+          .asyncExpand((event) => Stream.periodic(
+              const Duration(milliseconds: 300), (_) => _Action.b3).take(1)),
+      (action, getState) => Stream.error(Exception()),
+    ],
+    reducer: (state, action) {
+      switch (action) {
+        case _Action.b1:
+          return state + 1;
+        case _Action.b2:
+          return state + 2;
+        case _Action.b3:
+          return state + 3;
+        default:
+          return state;
+      }
+    },
+    handleError: (e, s) => print('Oh no $e'),
+  );
 
-    await Future.delayed(const Duration(seconds: 1));
-    print('<->[1] ${store.state}');
-    store.stateStream.listen((event) => print('[1] $event'));
+  await Future.delayed(const Duration(seconds: 1));
+  print('<~> [1] ${store.state}');
+  final sub = store.stateStream.listen((event) => print('~> [1] $event'));
 
-    store.dispatch(Action.a1);
-    store.dispatch(Action.a2);
-    store.dispatch(Action.a3);
+  store.dispatch(_Action.a1);
+  store.dispatch(_Action.a2);
+  store.dispatch(_Action.a3);
 
-    await Future.delayed(const Duration(seconds: 1));
+  await Future.delayed(const Duration(seconds: 1));
 
-    store.dispatch(Action.a1);
-    store.dispatch(Action.a2);
-    store.dispatch(Action.a3);
+  store.dispatch(_Action.a1);
+  store.dispatch(_Action.a2);
+  store.dispatch(_Action.a3);
 
-    print('<->[2] ${store.state}');
-    store.stateStream.listen((event) => print('[2] $event'));
+  await Future.delayed(const Duration(seconds: 1));
+  store.dispatch(_Action.b0);
+  store.dispatch(_Action.b0);
+  store.dispatch(_Action.b0);
 
-    await Future.delayed(const Duration(seconds: 1));
-
-    store.dispatch(Action.a3);
-    store.dispatch(Action.a2);
-    store.dispatch(Action.a1);
-
-    await store.dispose();
-  } catch (e, s) {
-    print(s);
-  }
+  print('dispose');
+  await sub.cancel();
+  await store.dispose();
 }
