7c5b0f4cca007b37ffefb743d5e0e4d1627d879e
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2020-08-20 21:30:27 +0700

    add counter.dart

---
+++ example/counter.dart
@@ -0,0 +1,48 @@
+import 'package:meta/meta.dart';
+import 'package:rx_redux/rx_redux.dart';
+
+@sealed
+abstract class Action {}
+
+class Increment implements Action {
+  final int number;
+
+  Increment(this.number);
+}
+
+class Decrement implements Action {
+  final int number;
+
+  Decrement(this.number);
+}
+
+class State {
+  final int value;
+
+  State(this.value);
+
+  @override
+  String toString() => 'State{value: $value}';
+}
+
+void main() async {
+  final store = RxReduxStore<Action, State>(
+    initialState: State(0),
+    sideEffects: [],
+    reducer: (state, action) {
+      if (action is Increment) return State(state.value + action.number);
+      if (action is Decrement) return State(state.value - action.number);
+      return state;
+    },
+  );
+
+  store.stateStream.listen(print);
+
+  await Stream.periodic(const Duration(seconds: 1), (i) => i)
+      .take(10)
+      .map((i) => i.isEven ? Increment(i) : Decrement(i))
+      .forEach(store.dispatch);
+
+  await Future(() {});
+  await store.dispose();
+}
