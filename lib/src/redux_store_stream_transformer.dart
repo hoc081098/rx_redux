ba1f52ed4579812def846e36328f025ca6358935
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-09-14 00:52:32 +0700

    2.4.0

--- lib/src/redux_store_stream_transformer.dart
+++ lib/src/redux_store_stream_transformer.dart
@@ -195,7 +195,9 @@ class ReduxStoreStreamTransformer<A, S> extends StreamTransformerBase<A, S> {
       controller = StreamController<S>(
         sync: true,
         onListen: onListen,
+        // ignore: avoid_function_literals_in_foreach_calls
         onPause: () => subscriptions?.forEach((s) => s.pause()),
+        // ignore: avoid_function_literals_in_foreach_calls
         onResume: () => subscriptions?.forEach((s) => s.resume()),
         onCancel: onCancel,
       );
